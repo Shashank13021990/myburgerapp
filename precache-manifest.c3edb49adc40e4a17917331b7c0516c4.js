ff24cae6f64168e5db65585e961400fec867ad2f
Shashank13021990 <shashankyadav.1990@gmail.com>
2019-07-24 16:57:06 +0100

    Updates

---
+++ precache-manifest.c3edb49adc40e4a17917331b7c0516c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f720e736ed6a7d7d0b4eb7a177d682a6",
+    "url": "/myburgerapp/index.html"
+  },
+  {
+    "revision": "a9766a7a293069478236",
+    "url": "/myburgerapp/static/css/main.d3426443.chunk.css"
+  },
+  {
+    "revision": "e683de0090e27f098611",
+    "url": "/myburgerapp/static/js/2.377a1c55.chunk.js"
+  },
+  {
+    "revision": "a9766a7a293069478236",
+    "url": "/myburgerapp/static/js/main.453a8da3.chunk.js"
+  },
+  {
+    "revision": "163054965ccc9a731ef0",
+    "url": "/myburgerapp/static/js/runtime~main.fb1e3edd.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/myburgerapp/static/media/burger-logo.b8503d26.png"
+  }
+]);
